daee9203254f5cf56e8e9d74504db6bb9b367185
muhammadmasud1 <mdmasudrana15020@gmail.com>
2023-09-07 11:22:31 +0600

    never gonna

--- css/script.js
+++ css/script.js
@@ -4,3 +4,35 @@ let showicon = document.querySelector('.close-icons');
 showicon.addEventListener('click', function(){
             head.classList.toggle('active');
 })
+
+
+
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+
+  setTimeout(() => {
+
+    text.textContent = "Developer";
+
+  }, 0);
+
+  setTimeout(() => {
+
+    text.textContent = "Freelancer";
+
+  }, 4000);
+
+  setTimeout(() => {
+
+    text.textContent = "Youtuber";
+
+  }, 8000);
+
+}
+
+
+
+textLoad();
+
+setInterval(textLoad, 12000);
